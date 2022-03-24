f054c4488a0fa5899658246134ec66aa0698cb32
jomeg <jomeg08@gmail.com>
2022-03-24 00:17:54 -0500

    Parte inicial del proyecto de convertidor de moneda.

---
+++ app/src/main/java/com/example/convertidormonedas/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.convertidormonedas;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertir(View view){
+        EditText et1 = findViewById(R.id.et1);
+        String pesosString = et1.getText().toString();
+        double pesos = Double.parseDouble(pesosString);
+        double dolares = pesos / 3772;
+        String dolaresString = String.format("%.2f",dolares);
+        String mensaje = "COP " + pesosString + " son USD " + dolaresString;
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
